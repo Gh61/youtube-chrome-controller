99c935cfdcc75c26a08ce883e3ff797fe6b00f88
Gh61 <t.zenkner@email.cz>
2017-10-29 20:54:32 +0100

    First extension commit - allow controlling youtube player through YT API functions from background.js

---
+++ Gh61.Youtube.Controller/Gh61.Youtube.Controller.Chrome/app/background.js
@@ -0,0 +1,41 @@
+﻿///<reference path="../scripts/_references.js" />
+
+var BG = (function () {
+	var self = this;
+	self.log = function(obj) {
+		console.log(obj);
+		var logEl = document.getElementById("log");
+		var txt = logEl.value;
+		txt += "\r\n" + obj;
+		logEl.value = txt;
+	}
+
+	self.sendCommand = function (commandName) {
+		// I am sending command to all tabs in all windows
+		chrome.windows.getAll({
+			populate: true
+		}, function (windows) {
+			for (var w = 0; w < windows.length; w++) {
+				for (var i = 0; i < windows[w].tabs.length; i++) {
+					var tab = windows[w].tabs[i];
+					if (tab.url.indexOf("youtube") > -1) {
+						var request = {
+							command: commandName
+						};
+						chrome.tabs.sendMessage(tab.id, request);
+					}
+				}
+			}
+		});
+	}
+
+	// Listening to messages from tab(s)
+	chrome.extension.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			self.log("Message accepted:");
+			self.log(JSON.stringify(request));
+		}
+	);
+
+	return self;
+})();
---
+++ Gh61.Youtube.Controller/Gh61.Youtube.Controller.Chrome/app/content_script.js
@@ -0,0 +1,140 @@
+﻿///<reference path="../scripts/_references.js" />
+
+(function (window, document) {
+	var self = this;
+	self.log = function(text) {
+		console.log("[YoutubeController] " + text);
+	}
+
+	self.executePageScript = function(fc) {
+		var actualCode = "(" + fc + ")();";
+		var script = document.createElement("script");
+		script.textContent = actualCode;
+		(document.head || document.documentElement).appendChild(script);
+		script.remove();
+	}
+
+	self.executePageScriptAndReturn = function (fc) {
+		// insert hidden input for result
+		var resultInput = document.createElement("input");
+		resultInput.type = "hidden";
+		resultInput.id = "content-script-result";
+		document.body.appendChild(resultInput);
+		
+		// execute script and set result to hidden element
+		var actualCode = "document.getElementById('content-script-result').value = (" + fc + ")();";
+		var script = document.createElement("script");
+		script.textContent = actualCode;
+		(document.head || document.documentElement).appendChild(script);
+		script.remove();
+
+		// get set value
+		var result = resultInput.value;
+		resultInput.remove();
+		return result;
+	}
+
+	self.player = {
+		// We need to pass through Youtube API functions
+		playVideo: function() {
+			self.executePageScript(function() {
+				document.getElementById("movie_player").playVideo();
+			});
+		},
+		pauseVideo: function() {
+			self.executePageScript(function () {
+				document.getElementById("movie_player").pauseVideo();
+			});
+		},
+		nextVideo: function() {
+			self.executePageScript(function () {
+				document.getElementById("movie_player").nextVideo();
+			});
+		},
+		previousVideo: function() {
+			self.executePageScript(function () {
+				document.getElementById("movie_player").previousVideo();
+			});
+		},
+		getPlayerState: function () {
+			/*
+			 * -1 - unstarted
+			 * 0 - ended
+			 * 1 - playing
+			 * 2 - paused
+			 * 3 - buffering
+			 * 5 - video cued
+			 */
+
+			return parseInt(self.executePageScriptAndReturn(function() {
+				return document.getElementById("movie_player").getPlayerState();
+			}));
+		}
+	};
+
+	self.createTabMessage = function(status) {
+		return {
+			tabStatus: status
+		}
+	};
+	self.notifyPlayerState = function () {
+		var status = "Playing";
+		var title = "";
+
+		var state = self.player.getPlayerState();
+		if (state === -1 || state === 2) {
+			status = "Paused";
+		}
+		var titleElem = document.getElementById("watch-headline-title");
+		if (titleElem != null) {
+			title = titleElem.children[0].children[0].textContent.trim();
+		}
+
+		var message = {
+			status: status,
+			title: title
+		}
+
+		chrome.runtime.sendMessage(message);
+	};
+
+	// Listening to commands from background.js
+	chrome.extension.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			switch(request.command) {
+				case "play":
+					self.player.playVideo();
+					break;
+				case "pause":
+					self.player.pauseVideo();
+					break;
+				case "playpause":
+					var state = self.player.getPlayerState();
+					self.log("PlayerState: " + state);
+					if (state === 1) { // playing => pause
+						self.player.pauseVideo();
+					} else if(state === 2) { // paused => play
+						self.player.playVideo();
+					}
+					break;
+				case "next":
+					self.player.nextVideo();
+					break;
+				case "prev":
+					self.player.previousVideo();
+					break;
+
+
+				case "stat":
+					// only request for player status
+					break;
+				default:
+					self.log("Unknown command '" + request.command + "'.");
+					break;
+			}
+
+			// After every command, sending player status (after 1s wait)
+			setTimeout(self.notifyPlayerState, 1000);
+		}
+	);
+})(window, document);
